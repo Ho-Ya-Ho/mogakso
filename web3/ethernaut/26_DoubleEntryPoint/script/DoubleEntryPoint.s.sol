e5dabd028c152d8716a1b6437d0c3143da0d6a37
Ho-Ya-Ho <rkdgh1056@gmail.com>
2025-08-15 14:29:58 +0900

    26_DoubleEntryPoint

--- web3/ethernaut/26_DoubleEntryPoint/script/DoubleEntryPoint.s.sol
+++ web3/ethernaut/26_DoubleEntryPoint/script/DoubleEntryPoint.s.sol
@@ -1,19 +1,36 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console} from "forge-std/Script.sol";
+import "../src/DoubleEntryPoint.sol";
 import {DoubleEntryPoint} from "../src/DoubleEntryPoint.sol";
+import {Script, console} from "forge-std/Script.sol";
 
 contract DoubleEntryPointScript is Script {
-    DoubleEntryPoint public counter;
+    DoubleEntryPoint public doubleEntryPoint;
 
-    function setUp() public {}
+    function setUp() public {
+        doubleEntryPoint = DoubleEntryPoint(0xaEdBDf054DaF39231c213b6F965270AB61683Df8);
+    }
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new DoubleEntryPoint();
+        Forta forta = doubleEntryPoint.forta();
+        FakeDetectionBot fakeDetectionBot = new FakeDetectionBot(address(forta));
+        forta.setDetectionBot(address(fakeDetectionBot));
 
         vm.stopBroadcast();
+    }
+}
+
+contract FakeDetectionBot is IDetectionBot {
+    IForta public forta;
+
+    constructor(address fortaAddress) {
+        forta = IForta(fortaAddress);
+    }
+
+    function handleTransaction(address user, bytes calldata) external override {
+        forta.raiseAlert(user);
     }
 }
