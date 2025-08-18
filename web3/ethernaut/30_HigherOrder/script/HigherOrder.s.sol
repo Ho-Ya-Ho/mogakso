80c9ff59a5ece01cbf18afff96d5094845d93ae0
Ho-Ya-Ho <rkdgh1056@gmail.com>
2025-08-18 21:20:39 +0900

    30_HigherOrder

--- web3/ethernaut/30_HigherOrder/script/HigherOrder.s.sol
+++ web3/ethernaut/30_HigherOrder/script/HigherOrder.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {HigherOrder} from "../src/HigherOrder.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract HigherOrderScript is Script {
+    HigherOrder public higherOrder;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        higherOrder = new HigherOrder();
 
         vm.stopBroadcast();
     }
