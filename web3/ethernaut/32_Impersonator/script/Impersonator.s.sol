9b1c10dce47b66903296e063dbb1327250fa0477
Ho-Ya-Ho <rkdgh1056@gmail.com>
2025-08-21 23:59:12 +0900

    32_Impersonator

--- web3/ethernaut/32_Impersonator/script/Impersonator.s.sol
+++ web3/ethernaut/32_Impersonator/script/Impersonator.s.sol
@@ -1,18 +1,27 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console} from "forge-std/Script.sol";
+import "../src/Impersonator.sol";
 import {Impersonator} from "../src/Impersonator.sol";
+import {Script, console} from "forge-std/Script.sol";
 
 contract ImpersonatorScript is Script {
-    Impersonator public impersonator;
-
-    function setUp() public {}
-
     function run() public {
         vm.startBroadcast();
 
-        impersonator = new Impersonator();
+        address instanceAddr = 0x77437C54960a6681eA7ccBB65389a2BbE71F1d41;
+
+        Impersonator impersonator = Impersonator(instanceAddr);
+        ECLocker locker = impersonator.lockers(0);
+
+        bytes32 r = 0x1932cb842d3e27f54f79f7be0289437381ba2410fdefbae36850bee9c41e3b91;
+        bytes32 s = 0x78489c64a0db16c40ef986beccc8f069ad5041e5b992d76fe76bba057d9abff2;
+        uint8 v = 27;
+
+        uint256 n = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141;
+        v = 28;
+
+        locker.changeController(v, r, bytes32(n - uint256(s)), address(0));
 
         vm.stopBroadcast();
     }
